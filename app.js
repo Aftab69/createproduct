0a276b5766b832aaefcde04fde7d84c352edac31
Aftab69 <aftabhssn3@gmail.com>
2022-11-19 15:30:21 +0530

    before deployment

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = require("./routes");
+const path = require("path");
+const app = express();
+app.use(express.json())
+require("./connection");
+app.use(router)
+
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./client/build/index.html"),
+    function (err) {
+      res.status(500).send(err);
+    }
+  );
+});
+
+app.listen(5000,
+    console.log("app running at port 5000")
+)
