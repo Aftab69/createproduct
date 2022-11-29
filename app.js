042a661e3e80a7f7fd692f2fe2e487af5b2400ae
Aftab69 <aftabhssn3@gmail.com>
2022-11-29 20:55:32 +0530

    cors added

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = require("./routes");
+const cors = require('cors');
 const path = require("path");
 const app = express();
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 require("./connection");
-app.use(router)
+app.use(router);
 
 app.use(express.static(path.join(__dirname, "./client/build")));
 app.get("*", function (_, res) {
